4d7051a32256b0547ccca84062f85d7811c2fb3f
genteanaranea <gentiaranea@gmail.com>
2020-02-16 23:34:57 +0000

    fix: div from main into abc44_a

---
+++ abc44_a.py
@@ -0,0 +1,10 @@
+# 高橋君とホテルイージー
+###
+# N=5
+# K=3
+# X=10000
+# Y=9000
+###
+def abc44_a():
+  n,k,x,y = [int(input()) for i in range(4)]
+  print(k*x+(n-k)*y)
--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-# 高橋君とホテルイージー
-###
-# N=5
-# K=3
-# X=10000
-# Y=9000
-###
-def abc44_a():
-  n,k,x,y = [int(input()) for i in range(4)]
-  print(k*x+(n-k)*y)
-
-abc44_a()
+from abc44_a import abc44_a
